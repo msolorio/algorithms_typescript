638ae41d044ad4589aecbc60096f22810a4b4d77
msolorio <msolorio.github@gmail.com>
2022-05-16 19:49:26 -0700

    Adds codewars algo

---
+++ vendors/codewars/order-words/index.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input String is empty, return an empty String. The words in the input String will only contain valid consecutive numbers.
+
+For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
+*/
+function getNumFromWord(word) {
+    const [num] = word.match(/[1-9]/) || ['0'];
+    return parseInt(num);
+}
+function orderWords(words) {
+    const wordsArr = words.split(' ');
+    const sortedWordsArr = wordsArr.sort((a, b) => {
+        const aNum = getNumFromWord(a);
+        const bNum = getNumFromWord(b);
+        return aNum - bNum;
+    });
+    return sortedWordsArr.join(' ');
+}
+module.exports = orderWords;
+//# sourceMappingURL=index.js.map
---
+++ vendors/codewars/order-words/index.spec.js
@@ -0,0 +1,38 @@
+const { expect } = require('chai');
+const orderWords = require('./index.js');
+
+console.log('hello from index.test.js');
+
+describe('orderWords', () => {
+  it('0 - should return a string', () => {
+    expect(orderWords('this1 is2 a3 test4')).to.be.a('string');
+  });
+
+  it('1 - should return return words ordered by included numbers', () => {
+    const input = 'T3his is2 4a tes1t';
+    const output = 'tes1t is2 T3his 4a';
+
+    expect(orderWords(input)).to.equal(output);
+  });
+
+  it('2 - should return words ordered correctly if only numbers', () => {
+    const input = '4 2 3 1';
+    const output = '1 2 3 4';
+
+    expect(orderWords(input)).to.equal(output);
+  });
+
+  it('3 - should return words ordered correctly if numbers in beginning of word', () => {
+    const input = '3a 2test 1this 4is';
+    const output = '1this 2test 3a 4is';
+
+    expect(orderWords(input)).to.equal(output);
+  });
+
+  it('4 - should return words ordered correctly if numbers at end of word', () => {
+    const input = 'a3 test2 this1 is4';
+    const output = 'this1 test2 a3 is4';
+
+    expect(orderWords(input)).to.equal(output);
+  });
+});
